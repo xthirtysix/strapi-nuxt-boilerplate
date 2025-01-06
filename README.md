36067ce271b21834a6bd4405545e9fd236f21a79
xthirtysix <ggalliosha@gmail.com>
2025-01-06 22:35:12 +0300

    ✨ feat: init monorepo

---
+++ README.md
@@ -0,0 +1,1 @@
+#Strapi Nuxt Boilerplate
---
+++ apps/client/nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  future: {
+    compatibilityVersion: 4,
+  },
+  devtools: { enabled: true },
+});
