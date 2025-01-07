941228d4dcbe0114f39864835014dbb59389a029
xthirtysix <ggalliosha@gmail.com>
2025-01-07 09:11:03 +0300

    ♻️ refactor(client): support fsd folder structure

--- apps/client/nuxt.config.ts
+++ apps/client/nuxt.config.ts
@@ -1,8 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  future: {
-    compatibilityVersion: 4,
-  },
-  devtools: { enabled: true },
-});
+    compatibilityDate: '2024-11-01',
+    future: {
+        compatibilityVersion: 4,
+    },
+    devtools: { enabled: true },
+    srcDir: 'src',
+    dir: {
+        app: 'app',
+        pages: 'app/routes',
+        layouts: 'app/layouts',
+    },
+})
---
+++ apps/client/src/app/app.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1>Hello</h1>
+        <nuxt-route-announcer />
+        <nuxt-layout>
+            <nuxt-page />
+        </nuxt-layout>
+    </div>
+</template>
